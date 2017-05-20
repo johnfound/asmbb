ee4faefd8bf738da7d1ac40cbf543f51bd6e5c23
johnfound <johnfound@noemail.net>
2017-05-20 06:49:31 +0000

    Preparation for a release. Small fix of the Light theme.
    FossilOrigin-Name: d47da02d952e8ff145415d704bf82a77585e1892

--- install/create_release.sh
+++ install/create_release.sh
@@ -2,72 +2,27 @@
 
 mkdir asmbb
 
-mkdir asmbb/templates/
-mkdir asmbb/templates/.images/
-mkdir asmbb/templates/.images/emoticons/
-mkdir asmbb/templates/.images/chatemoticons/
-
-mkdir asmbb/templates/Light/
-mkdir asmbb/templates/Light/.images/
-mkdir asmbb/templates/Light/.images/emoticons/
-mkdir asmbb/templates/Light/.images/chatemoticons/
-
-mkdir asmbb/templates/Wasp/
-mkdir asmbb/templates/Wasp/.images/
-mkdir asmbb/templates/Wasp/.images/emoticons/
-mkdir asmbb/templates/Wasp/.images/chatemoticons/
-
-mkdir asmbb/images/
-mkdir asmbb/images/favicons/
-
 # complile less files
 
-cd ../www/templates/
-./compile_styles.sh
+pushd .
 
-cd Wasp/
+cd ../www/templates/Wasp/
 ./compile_styles.sh
 
 cd ../Light/
 ./compile_styles.sh
 
-cd ../../../install/
+popd
 
-# engine files
+# copy engine files
 cp ../www/engine asmbb/
 cp ../www/*.so asmbb/
 
-# the templates and style files for the default theme.
-cp ../www/templates/*.css asmbb/templates/
-cp ../www/templates/*.tpl asmbb/templates/
+# copy images
+rsync -ar ../www/images/* asmbb/images/ --exclude-from=exclude.txt
 
 # templates and styles for Light theme
-cp ../www/templates/Light/*.css asmbb/templates/Light/
-cp ../www/templates/Light/*.tpl asmbb/templates/Light/
-
-# templates and styles for Wasp theme
-cp ../www/templates/Wasp/*.css asmbb/templates/Wasp/
-cp ../www/templates/Wasp/*.tpl asmbb/templates/Wasp/
-
-# images
-
-cp ../www/images/favicon.ico asmbb/images/
-cp ../www/images/favicons/*.* asmbb/images/favicons/
-
-# default skin
-cp ../www/templates/.images/*.* asmbb/templates/.images/
-cp ../www/templates/.images/emoticons/*.* asmbb/templates/.images/emoticons/
-cp ../www/templates/.images/chatemoticons/*.* asmbb/templates/.images/chatemoticons/
-
-# Wasp skin
-cp ../www/templates/Wasp/.images/*.* asmbb/templates/Wasp/.images/
-cp ../www/templates/Wasp/.images/emoticons/*.* asmbb/templates/Wasp/.images/emoticons/
-cp ../www/templates/Wasp/.images/chatemoticons/*.* asmbb/templates/Wasp/.images/chatemoticons/
-
-# Light skin
-cp ../www/templates/Light/.images/*.* asmbb/templates/Light/.images/
-cp ../www/templates/Light/.images/emoticons/*.* asmbb/templates/Light/.images/emoticons/
-cp ../www/templates/Light/.images/chatemoticons/*.* asmbb/templates/Light/.images/chatemoticons/
+rsync -ar ../www/templates/* asmbb/templates/ --exclude-from=exclude.txt
 
 # example config files for apache and lighttpd
 cp .htaccess asmbb/
