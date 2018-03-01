376f9fa86d0417c4cc68105900d5e808beb0cecf
johnfound <johnfound@noemail.net>
2018-03-01 08:34:36 +0000

    Fixed minor bug in thread attributes editing.
    FossilOrigin-Name: 0c12b88bb119dde4c1e2982933907e327c7ac1c9

--- source/thread_attr.sql
+++ source/thread_attr.sql
@@ -9,6 +9,15 @@ select
     where
       threadid=id
   ) as tags,
+  ( select
+      userid
+    from
+      posts
+    where
+      threadid = threads.id
+    order by posts.id
+    limit 1
+  ) as userid,
   Pinned
 from
   threads
