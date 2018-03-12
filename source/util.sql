0c545d22998a8fad263eafcc9bfd007c02f38a3c
johnfound <johnfound@noemail.net>
2018-03-12 15:58:04 +0000

    Work on implementing some help texts in the forums templates.
    FossilOrigin-Name: 2ca390334f55943b38483cf51f0041f8f12a55f8

--- source/util.sql
+++ source/util.sql
@@ -1,5 +1,57 @@
 /* This file contains some useful scripts for administration of AsmBB forum using the SQLite console. */
 
+
+-- Displays all guests that downloaded only files like images, robots.txt, etc. without actually visiting the website.
+
+select
+  (g.addr >> 24 & 255)||'.'||(g.addr >> 16 & 255)||'.'||(g.addr >> 8 & 255)||'.'||(g.addr & 255) as IP,
+  datetime(g.LastSeen, 'unixepoch') as Time,
+  gr.method,
+  gr.request,
+  gr.client,
+  gr.referer
+from
+  guests g
+left join
+  guestrequests gr on g.addr = gr.addr
+where
+  g.addr not in (select remoteIP from UserLog);
+
+
+-- Displays a list of unique referers with counts of all visitors from that URL:
+
+select count() as cnt, substr(referer, 1, instr(referer, '?')-1) as refr from guestrequests where refr is not null and refr not like '%board.asm32.info%' and instr(referer, '?') > 0
+group by refr
+union
+select count() as cnt, referer as refr from guestrequests where refr is not null and refr not like '%board.asm32.info%' and instr(referer, '?') = 0
+group by refr
+order by cnt desc;
+
+-- the same, but faster
+
+select
+  count(refr) as cnt,
+  refr
+from
+  ( select
+      substr(referer, 1, instr(referer, '?')-1) as refr
+    from
+      guestrequests
+    where
+      referer not like '%board.asm32.info%' and instr(referer, '?') > 0
+    union all
+    select
+      referer as refr
+    from
+      guestrequests
+    where
+       referer not like '%board.asm32.info%' and instr(referer, '?') = 0
+  )
+group by refr
+order by cnt desc, refr;
+
+
+
 -- Displays a report about the Guests active from the last 5 minutes.
 
 select datetime(LastSeen, 'unixepoch') as Date, (addr >> 24 & 255)||'.'||(addr >> 16 & 255)||'.'||(addr >> 8 & 255)||'.'||(addr & 255) as IP, Client
@@ -18,7 +70,7 @@ left join
   guestrequests gr on g.addr = gr.addr
 where
   g.rowid in (select rowid from guests order by rowid desc limit 200) and
-  IP <> '149.62.201.127';  -- this is the current my IP.
+  IP <> '127.0.0.1';
 
 /* Rebuilds the full text search table */
 
--- www/templates/Wasp/form_new_thread.tpl
+++ www/templates/Wasp/form_new_thread.tpl
@@ -12,7 +12,7 @@
       <p>Title:</p>
       <input class="title" type="edit" value="[caption]" placeholder="Thread title" name="title" autofocus>
     </div><div class="edit_groupR">
-      <p>Tags: <span class="small">(max 3, comma delimited, no spaces)</span> [case:[special:dir]| |+ "[special:dir]"]</p>
+      <p>Tags: <span class="small">(max 3, comma delimited, no spaces)</span> [case:[special:dir]| |+ "[special:dir]"]<img src="[special:skin]/</p>
       <input class="tags"  type="edit" value="[tags]" name="tags" placeholder="some tags here">
     </div>
     <p>Post content:</p>
@@ -20,7 +20,7 @@
     <div class="panel">
       <input type="submit" name="submit" value="Submit" >
       <input type="submit" name="preview" value="Preview" >
-      <input type="hidden" name="ticket" value="[Ticket]" >
+      <input type="hidden" name="ticket" value="[Ticketl]" >
       <input type="reset" value="Revert" >
     </div>
   </form>
