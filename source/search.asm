d11fd98306f3d9c89cf558639bf38600342b2e54
johnfound <johnfound@noemail.net>
2018-08-26 11:43:14 +0000

    Make search engine to respect limited access threads.
    FossilOrigin-Name: 665e67d2628b010b069c7b76ee3a28888093e931

--- source/search.asm
+++ source/search.asm
@@ -1,29 +1,8 @@
 
+sqlSearchCntPrefix  StripText "search_cnt.sql", SQL
+sqlSearchPrefix     StripText "search.sql", SQL
 
-;sqlSearchCnt    StripText "search_cnt.sql", SQL
-;sqlSearchPrefix  StripText "search.sql", SQL
-
-sqlSearchPrefix text    "select ",                                                                                              \
-                        "  PostFTS.rowid, ",                                                                                    \
-                        "  PostFTS.user as UserName, ",                                                                         \
-                        "  P.userID, ",                                                                                         \
-                        "  U.av_time as AVer, ",                                                                                \
-                        "  PostFTS.slug, ",                                                                                     \
-                        "  strftime('%d.%m.%Y %H:%M:%S', P.postTime, 'unixepoch') as PostTime, ",                               \
-                        "  PC.count as ReadCount, ",                                                                            \
-                        "  snippet(PostFTS, 0, '*', '*', '...', 16) as Content, ",                                              \
-                        "  PostFTS.Caption, ",                                                                                  \
-                        "  (select count() from UnreadPosts UP where UP.UserID = ?4 and UP.PostID = PostFTS.rowid) as Unread ", \
-                        "from ",                                                                                                \
-                        "  PostFTS ",                                                                                           \
-                        "  left join Posts P on P.id = PostFTS.rowid ",                                                         \
-                        "  left join PostCnt PC on PC.postID = P.id ",                                                          \
-                        "  left join Users U on U.id = P.userID "
-
-
-sqlSearchCntPrefix text "select count() from PostFTS "
-
-sqlSearchWhere text    " where PostFTS match  ?1 "
+sqlSearchWhere text    " where PostFTS match ?1 and ((LT.userid is null or LT.userid = ?4))"
 sqlSearchLimit text    " limit ?2 offset ?3 "
 
 
@@ -193,6 +172,7 @@ begin
         je      .match_ok
 
         cinvoke sqliteBindText, [.stmt], 1, eax, [eax+string.len], SQLITE_STATIC
+        cinvoke sqliteBindInt,  [.stmt], 4,  [esi+TSpecialParams.userID]
 
 .match_ok:
 
