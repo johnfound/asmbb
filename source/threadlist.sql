cc70a89913dee4533b2e90aef7a970a6ccbc521d
johnfound <johnfound@noemail.net>
2018-09-16 06:25:43 +0000

    fixed broken query.
    FossilOrigin-Name: d2be147867ebd3b4eb1e7e4967058a6d802e2a31

--- source/threadlist.sql
+++ source/threadlist.sql
@@ -5,17 +5,17 @@ select
   Caption,
   Pinned,
   strftime('%d.%m.%Y %H:%M:%S', LastChanged, 'unixepoch') as TimeChanged,
-  (select count() from posts where threadID = T.id) as PostCount,
-  (select count() from posts, UnreadPosts U where id = PostID and threadID = T.id and U.userID = ?3 ) as Unread,
-  (select PostID from posts, UnreadPosts U where id = PostID and threadID = T.id and U.userID = ?3 limit 1) as FirstUnread,
-  (select Count from PostCnt where postid = (select id from Posts where threadID = T.id limit 1)) as ReadCount,
-  (select group_concat('<li><a href="/!userinfo/'||nick||'">'||nick||'</a></li>','') from (select url_encode(nick) as nick from threadposters left join users on userID = id where threadid = T.id order by firstPost)) as Posters,
-  (select group_concat('<li><a href="/!userinfo/'||url_encode(nick)||'">'||nick||'</a></li>','') from LimitedAccessThreads left join Users on id = userid where threadID = T.id) as Invited,
+  (select count() from posts P where P.threadID = T.id) as PostCount,
+  (select count() from posts P, UnreadPosts U where id = PostID and P.threadID = T.id and U.userID = ?3 ) as Unread,
+  (select PostID from posts P, UnreadPosts U where id = PostID and P.threadID = T.id and U.userID = ?3 limit 1) as FirstUnread,
+  (select Count from PostCnt where postid = (select id from Posts P where P.threadID = T.id limit 1)) as ReadCount,
+  (select group_concat('<li><a href="/!userinfo/'||nick||'">'||nick||'</a></li>','') from (select url_encode(nick) as nick from threadposters TP left join users on userID = id where TP.threadid = T.id order by firstPost)) as Posters,
+  (select group_concat('<li><a href="/!userinfo/'||url_encode(nick)||'">'||nick||'</a></li>','') from LimitedAccessThreads LA left join Users on id = userid where LA.threadID = T.id) as Invited,
   (select group_concat('<li><a href="/'||url_encode(html_encode(TT.tag))||'/" title="'||html_encode(T.description)||'">'||html_encode(TT.tag)||'</a></li>','') from ThreadTags TT left join Tags T on T.tag = TT.tag where TT.threadID=T.id) as ThreadTags,
 [case:[special:isadmin]|
   LT.userid
 |
-  exists (select 1 from LimitedAccessThreads where threadid = T.id)
+  exists (select 1 from LimitedAccessThreads LA where LA.threadid = T.id)
 ] as limited
 
 from
