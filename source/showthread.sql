0f50471d4e3e4a6a0214fc5bfbe594ce08de97c8
johnfound <johnfound@noemail.net>
2018-09-16 06:57:00 +0000

    Better naming in the query.
    FossilOrigin-Name: e71c601eb75341c2550a073d8860cd63b8061d22

--- source/showthread.sql
+++ source/showthread.sql
@@ -12,11 +12,15 @@ select
   U.PostCount as UserPostCount,
   U.av_time as AVer,
   ?4 as Slug,
-  exists (select 1 from UnreadPosts UP where UP.UserID = ?5 and UP.PostID = P.id limit 1) as Unread,
-  exists (select 1 from PostsHistory PH where PH.postID = P.id limit 1) as HistoryCount,
+  exists (select 1 from UnreadPosts UP where UP.UserID = ?5 and UP.PostID = P.id) as Unread,
+  exists (select 1 from PostsHistory PH where PH.postID = P.id) as HistoryFlag,
   PC.count as ReadCount
 
-from Posts P
-left join PostCNT PC on PC.postid = P.id
-left join Users U on U.id = P.userID
-where P.id in (select id from posts where threadid=?1 limit ?2 offset ?3);
+from
+  Posts P
+left join
+  PostCNT PC on PC.postid = P.id
+left join
+  Users U on U.id = P.userID
+where
+  P.id in (select id from posts where threadid=?1 limit ?2 offset ?3);
