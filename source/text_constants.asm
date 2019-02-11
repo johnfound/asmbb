b1d2efb38a9bb4bdaef8751311af0aa7dce1635e
johnfound <johnfound@noemail.net>
2019-02-11 15:15:30 +0000

    Added french translations to the text strings, thanks to user macadoum from the forum.
    FossilOrigin-Name: bf982b92ddd7eea8784730fe0223402783c7f460

--- source/text_constants.asm
+++ source/text_constants.asm
@@ -1,73 +1,92 @@
 cUsersOnlineTitle       itext   EN: "Online Users",             \
                                 RU: "Активные потребители",     \
-                                BG: "Активни потребители"
+                                BG: "Активни потребители",      \
+                                FR: "Utilisateurs en ligne"
 
 cPostDeleteTitle        itext   EN: "Delete confirmation",       \
                                 RU: "Подтверждение удаления",    \
-                                BG: "Потвърждаване на изтриване"
+                                BG: "Потвърждаване на изтриване",\
+                                FR: "Confirmer la suppression"
 
 cPostRestoreTitle       itext   EN: "Restore to old version", \
                                 RU: "Восстановить предыдущую версию", \
-                                BG: "Възстановяване до предна версия"
+                                BG: "Възстановяване до предна версия",\
+                                FR: "Restaurer l’ancienne version"
 
 cHistoryTitle           itext   EN: "Post edition history",     \
                                 RU: "История редакции",         \
-                                BG: "История на редакциите"
+                                BG: "История на редакциите",    \
+                                FR: "Historique d’édition"
 
 
 cForumSettingsTitle     itext   EN: "Forum settings page",      \
                                 RU: "Настройки форума",         \
-                                BG: "Настройки на форума"
+                                BG: "Настройки на форума",      \
+                                FR: "Paramètres du forum"
 
 cPostingInTitle         itext   EN: "Posting in: ",             \
                                 RU: "Написать в: ",             \
-                                BG: "Публикация в: "
+                                BG: "Публикация в: ",           \
+                                FR: "Posté dans :"
 
 
 cSearchResultsTitle     itext   EN: "Search results for: ",     \
                                 RU: "Результаты поиска: ",      \
-                                BG: "Резултати от търсенето на: "
+                                BG: "Резултати от търсенето на: ", \
+                                FR: "Résultats de la recherche :"
 
 cNewThreadTitle         itext   EN: "New thread posting",       \
                                 RU: "Новая тема",               \
-                                BG: "Нова тема"
+                                BG: "Нова тема",                \
+                                FR: "Poster un nouveau message"
 
 cLoginDialogTitle       itext   EN: "Login",                    \
                                 RU: "Вход",                     \
-                                BG: "Включване"
+                                BG: "Включване",                \
+                                FR: "Connexion"
+
 
 cUserProfileTitle       itext   EN: "Profile for: ",            \
                                 RU: "Профиль потребителя: ",    \
-                                BG: "Потребителски профил: "
+                                BG: "Потребителски профил: "    \
+                                FR: "Profil de :"
 
 cEditingPageTitle       itext   EN: "Editing page: ",           \
                                 RU: "Редактирование: ",         \
-                                BG: "Редактиране на: "
+                                BG: "Редактиране на: ",         \
+                                FR: "Éditer la page:"
 
 cEditingThreadTitle     itext   EN: "Editing thread: ",         \
                                 RU: "Редактирование темы: ",    \
-                                BG: "Редактиране на тема: "
+                                BG: "Редактиране на тема: ",    \
+                                FR: "Éditer le sujet:"
 
 cSQLiteConsoleTitle     itext   EN: "WARNING! SQLite console. You can destroy your database here!",     \
                                 RU: "ВНИМАНИЕ! Конзоль SQLite. Возможно повреждение базы данных!",      \
-                                BG: "ВНИМАНИЕ! SQLite конзола. От тук е възможно да повредите базата данни!"
+                                BG: "ВНИМАНИЕ! SQLite конзола. От тук е възможно да повредите базата данни!",    \
+                                FR: "ATTENTION ! Console SQLite. Vous pouvez détruire votre base de données !"
 
 cCreateAdminTitle       itext   EN: "Create the admin account!",                \
-                                RU: "Создание учетную запись администратора!",   \
-                                BG: "Създаване на потребител администратор!"
+                                RU: "Создание учетную запись администратора!",  \
+                                BG: "Създаване на потребител администратор!",   \
+                                FR: "Créer le compte administrateur !"
 
 cThreadListTitle        itext   EN: "Threads list ",    \
                                 RU: "Темы ",            \
-                                BG: "Теми "
+                                BG: "Теми ",            \
+                                FR: "Liste des sujets"
 
 cChatTitle              itext   EN: "Chat ",            \
                                 RU: "Чат ",             \
-                                BG: "Чат "
+                                BG: "Чат ",             \
+                                FR: "Tchat"
 
 cAnonName               itext   EN: "Anon",             \
                                 RU: "Анон",             \
-                                BG: "Анон"
+                                BG: "Анон",             \
+                                FR: "Anon"
 
-cEmptySearch            itext   EN: "The search found nothing. Try another keywords.",      \
-                                RU: "Поиск не нашел ничего. Попробуйте другой запрос.",      \
-                                BG: "Търсенето не намери нищо. Опитайте с други думи."
+cEmptySearch            itext   EN: "The search found nothing. Try another keywords.",                  \
+                                RU: "Поиск не нашел ничего. Попробуйте другой запрос.",                 \
+                                BG: "Търсенето не намери нищо. Опитайте с други думи.",                 \
+                                FR: "La recherche n’a rien renvoyer. Essayer d’autres mots-clés."
