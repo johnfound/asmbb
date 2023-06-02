4405b70022d2f08ba806714d82fa65bb50d0de90
johnfound <>
2023-06-02 19:00:53 +0000

    Small fix in create_tar.sh
    FossilOrigin-Name: dbf080b611a92a5e25181de2e94bcff46d4bd263

--- www/templates/create_tar.sh
+++ www/templates/create_tar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm ./templates.tar.gz
-tar --exclude=*.less --exclude="_source" -czf ./templates.tar.gz ./_fonts/ ./Light/ ./mobile/ ./Wasp/ ./MoLight/ ./"Urban Sunrise"/
+tar --exclude=*.less --exclude=*.sh --exclude="_source" -czf ./templates.tar.gz ./_fonts/ ./Light/ ./mobile/ ./Wasp/ ./MoLight/ ./"Urban Sunrise"/
